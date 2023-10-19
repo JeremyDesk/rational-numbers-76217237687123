d0ce130d09225f17cf2db6f60cf5f66fb7fa1f17
dmartielli <dmartielli@thegraysonschool.org>
2023-10-19 15:14:11 -0400

    first commit

--- main.py
+++ main.py
@@ -10,15 +10,14 @@ class rational:
 
     def __str__(self):
         return f"{self.num}/{self.den}"
-
     def simplify(self):
         idk = math.gcf(self.num,self.den)
         self.num = self.num/idk
         self.den = self.dem/idk
 
 def main():
-    pass
-
+    fraction=(2,4)
+    print(fraction)
 
 if __name__ == '__main__':
     main()
