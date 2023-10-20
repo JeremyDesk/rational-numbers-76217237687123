1c65cecd61134e1d3a27ba4b9a66f71685742611
dmartielli <dmartielli@thegraysonschool.org>
2023-10-20 08:10:03 -0400

    first commit

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ class rational:
         if den != 0:
             self.num = num
             self.den = den
-            self.simplify(self)
+            self.simplify(num,den)
         else:
             raise ValueError("mmmm no no no; nuh-uh ")
 
     def __str__(self):
         return f"{self.num}/{self.den}"
-    def simplify(self):
-        idk = math.gcf(self.num,self.den)
-        self.num = self.num/idk
-        self.den = self.dem/idk
+    def simplify(self,num,den):
+        idk = math.gcd(self.num,self.den)
+        self.num = int(self.num/idk)
+        self.den = int(self.den/idk)
 
 def main():
-    fraction=(2,4)
+    fraction=rational(2,0)
     print(fraction)
 
 if __name__ == '__main__':
