137e407665adaf88b203383423021fbdd4cecfd0
dmartielli <dmartielli@thegraysonschool.org>
2023-10-24 14:35:52 -0400

    first commit

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import math
 class rational:
     def __init__(self, num, den):
+        if type(den) is not int:
+            raise ValueError("that uhhh denominator there isnt and integer 🤦")
+        if type(num) is not int:
+            raise ValueError("that uhhh numererator there isnt and integer 🤦")
         if den != 0:
             self.num = num
             self.den = den
-            self.simplify(num,den)
+            self.simplify(num, den)
         else:
             raise ValueError("mmmm no no no; nuh-uh ")
 
+
+
     def __str__(self):
-        return f"{self.num}/{self.den}"
+        if self.num != 0:
+            return f"{self.num}/{self.den}"
+        else:
+            return "0"
     def simplify(self,num,den):
         idk = math.gcd(self.num,self.den)
         self.num = int(self.num/idk)
         self.den = int(self.den/idk)
 
 def main():
-    fraction=rational(2,0)
+    fraction=rational(int(input("numerator:")),int(input("denominator:")))
     print(fraction)
 
 if __name__ == '__main__':
